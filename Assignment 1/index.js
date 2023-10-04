d6f3c1283fd82793d5d3ea0c1c74243db72ad672
Tinatin <96499145+tikoration@users.noreply.github.com>
2023-10-04 08:48:29 +0000

    first commit

---
+++ Assignment 1/index.js
@@ -0,0 +1,38 @@
+
+
+function isEqual(a, b){
+    if(typeof a === typeof b && a == b){
+        return "a is equal to b"
+    }else{
+        return "a is not equal to b"
+    }
+}
+
+function fahrenToCels(fahrenheit){
+    if(typeof fahrenheit !== 'number'){
+        return false
+    }
+    const celsius = (fahrenheit - 32)*5/9
+    return celsius
+}
+
+function calculator(a,b,operation){
+    if(typeof a !== 'number' || typeof b !== "number"){
+        return false
+    }
+    if(operation === '/'){
+        return a/b
+    }else if(operation === '*'){
+        return a*b
+    }else if(operation === "+"){
+        return a+b
+    }else if(operation === "-"){
+        return a-b
+    }
+    return false
+}
+
+console.log(isEqual("12",12))
+console.log(fahrenToCels(36))
+console.log(calculator(12,2,'/'))
+
