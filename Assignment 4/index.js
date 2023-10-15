7f6a01d8da34d1f93946a73d80e1808adc2d4455
Tinatin <96499145+tikoration@users.noreply.github.com>
2023-10-15 16:55:34 +0000

    Assignment 4 upload

---
+++ Assignment 4/index.js
@@ -0,0 +1,30 @@
+// Task 1
+
+const replaceFunc = (string, valueToReplace, valueToReplaceWith) => {
+    const newStr = string.split(valueToReplace).join(valueToReplaceWith)
+    return newStr
+}
+
+console.log(replaceFunc("I like flowers", "flowers", "plants"))
+
+// Task 2
+
+const upSentence = (string) => {
+    const newStr = string.split('')
+    .map(letter => letter.toUpperCase())
+    .join('')
+    return newStr
+}
+
+console.log(upSentence("hello"))
+
+// Task 3
+
+const users = [{name: 'Lasha', age: 30}, {name: 'Saba', age: 20}]
+
+const sortByAge = (arr) => {
+    const sorted = arr.sort((a,b) => a.age - b.age)
+    return sorted
+}
+
+console.log(sortByAge(users))
