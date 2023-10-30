82232511a379e3c5528ae7074037b010c1c41254
tikoration <tinatinmghebrishvili@gmail.com>
2023-10-31 00:33:30 +0400

    Assignment 8 upload

---
+++ Assignment_8/main.js
@@ -0,0 +1,82 @@
+// Task 1 
+
+const expo = (number, power, callback) => { 
+    if(power === 0) return 1 
+    return callback(number, expo(number, power - 1, callback), callback)
+} 
+
+console.log(expo(3,4,(a,b) => a*b)) 
+
+// Task 2 
+
+const divEl = document.querySelector(".users")
+
+fetch('https://jsonplaceholder.typicode.com/posts') 
+.then(res => res.json()) 
+.then(data => {
+    data.forEach(el => {
+        const element = document.createElement('div')
+        element.innerHTML = `
+            <h2>${el.title}</h2>
+            <h3>${el.body}</h3>`
+        divEl.appendChild(element)
+    })
+}) 
+
+// Task 3 
+
+const cloneObj = (obj) => {
+    const newObj = {}
+    for(let key in obj){
+        if(typeof obj[key] !== "object" || obj[key] === null){
+            newObj[key] = obj[key]
+        } else {
+            if(Array.isArray(obj[key])){
+                newObj[key] = obj[key].map((el) => {
+                    if(typeof el !== "object"){
+                        return el
+                    } else {
+                        return cloneObj(el)
+                    }
+                })
+            }else {
+                newObj[key] = cloneObj(obj[key])
+            }
+        }
+    }
+    return newObj
+ } 
+ 
+ const deepCopy = (obj) => { 
+    return new Promise((resolve, reject) => { 
+        if(typeof obj === "object"){  
+            resolve(cloneObj(obj)) 
+        }else{ 
+            reject("This is not an object") 
+        } 
+    }) 
+} 
+
+const example = {
+    animal: "cat",
+    city: "Tbilisi", 
+    array: ["array", "of", "things"], 
+    object: {
+        color: "red",
+        shape: "circle",
+        anotherObj: {
+            what: "else",
+            should: "I",
+            write: "?"
+        }
+    },
+    lets: [
+        {try: 'this'},
+        {version: "shall"},
+        {we: "?"}
+    ]
+}
+
+deepCopy(example)
+.then((res) => console.log(res)) 
+.catch((err) => console.log(err))
