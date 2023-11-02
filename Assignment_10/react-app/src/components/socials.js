1ba549fdecada5bf3fac80e5efaad143e9025ccc
tikoration <tinatinmghebrishvili@gmail.com>
2023-11-02 22:50:16 +0400

    css changes made

--- Assignment_10/react-app/src/components/socials.js
+++ Assignment_10/react-app/src/components/socials.js
@@ -2,8 +2,12 @@
 export default function Socials(){
     return(
         <div className="socials">
-            <a href="https://github.com/tikoration" rel="noreferrer" target="_blank">Github</a>
-            <a href="https://www.linkedin.com/in/tinatin-mgebrishvili-789345171" rel="noreferrer" target="_blank">LinkedIn</a>
+            <a href="https://github.com/tikoration" rel="noreferrer" target="_blank">
+                <i className="fa-brands fa-github fa-2xl"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/tinatin-mgebrishvili-789345171" rel="noreferrer" target="_blank">
+                <i className="fa-brands fa-linkedin fa-2xl"></i>
+            </a>
         </div>
     )
 }
