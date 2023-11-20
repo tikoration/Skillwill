d2359b9252dc86bd2b2997cee3f3cb5743f8fb30
tikoration <tinatinmghebrishvili@gmail.com>
2023-11-20 21:10:37 +0400

    updated code, added hooks and some css

--- Assignment_15/toggle-theme/src/components/toggler.jsx
+++ Assignment_15/toggle-theme/src/components/toggler.jsx
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react"
+import useLocalStorage from "../hooks/useLocalStorage"
+import useWindowResize from "../hooks/useWindowResize"
 
 const Toggler = () => {
 
-    const [isDark, setIsDark] = useState(false)
+    const [isDark, setIsDark] = useLocalStorage('tab',false)
     const [theme, setTheme] = useState("")
+    const {width} = useWindowResize()
 
     const handleClick= () => {
-        setIsDark(prevState => !prevState)
-        console.log(isDark)
+        if(width > '481'){
+            setIsDark(prevState => !prevState)
+        }
     }
+
+    useEffect(() => {
+       if(width < '481'){
+        setIsDark(false)
+       }
+    }, [setIsDark, width])
 
     useEffect(() => {
         const themeStyle = isDark ? "dark-mode" : "light-mode"
@@ -21,7 +31,7 @@ const Toggler = () => {
                 Toggle Theme
             </button>
         </div>
-    )
+    ) 
 }
 
 export default Toggler
